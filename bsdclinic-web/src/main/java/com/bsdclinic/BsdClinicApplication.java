22b7ae160e3bf5217f30d53e17f293a2a0dea697
nguyendaitriet96 <triet.nguyendai@smartdev.com>
2025-04-24 18:00:24 +0700

    BSD-22: Add assets for admin page

--- bsdclinic-web/src/main/java/com/bsdclinic/BsdClinicApplication.java
+++ bsdclinic-web/src/main/java/com/bsdclinic/BsdClinicApplication.java
@@ -2,11 +2,6 @@ package com.bsdclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
-
-import java.util.Locale;
 
 @SpringBootApplication
 public class BsdClinicApplication {
@@ -15,10 +10,4 @@ public class BsdClinicApplication {
 		SpringApplication.run(BsdClinicApplication.class, args);
 	}
 
-	@Bean
-	public LocaleResolver localeResolver() {
-		AcceptHeaderLocaleResolver resolver = new AcceptHeaderLocaleResolver();
-		resolver.setDefaultLocale(Locale.ENGLISH);
-		return resolver;
-	}
 }

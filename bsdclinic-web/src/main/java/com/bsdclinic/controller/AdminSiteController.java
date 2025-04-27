9556df54c5920a0cee19ce35c0e8d23aeaa1ce7e
DESKTOP-6LG62K8\Triet_PC <triet9510@gmail.com>
2025-04-27 21:41:04 +0700

    BSD-46: Disable _en and update login direction

--- bsdclinic-web/src/main/java/com/bsdclinic/controller/AdminSiteController.java
+++ bsdclinic-web/src/main/java/com/bsdclinic/controller/AdminSiteController.java
@@ -4,11 +4,17 @@ import com.bsdclinic.UserPrincipal;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AdminSiteController {
-    @GetMapping({"/login", "/admin"})
+    @GetMapping("/login")
     public String toLoginPage(@AuthenticationPrincipal UserPrincipal principal) {
-        return principal != null ? "admin/index" : "admin/auth/login";
+        return principal != null ? "redirect:/admin" : "admin/auth/login";
+    }
+
+    @GetMapping("/admin")
+    public ModelAndView toHomePage() {
+        return new ModelAndView("admin/index");
     }
 }

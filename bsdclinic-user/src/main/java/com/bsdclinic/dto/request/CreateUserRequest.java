94b45d6a38affa6a7118120ea1a4720320669285
trungphan1357 <trungphan1357@gmail.com>
2025-04-29 21:36:28 +0700

    BDS-54: Update create USER

--- bsdclinic-user/src/main/java/com/bsdclinic/dto/request/CreateUserRequest.java
+++ bsdclinic-user/src/main/java/com/bsdclinic/dto/request/CreateUserRequest.java
@@ -10,27 +10,27 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CreateUserRequest {
-    @RuleAnnotation.ExistedEmail( message = "Email đã tồn tại")
-    @NotBlank(message = "Email không được để trống")
-    @Size(max = 255, message = "Chỉ nhập tối đa 255 kí tự")
-    @Pattern(regexp = "[\\w.%+-]+@[\\w.-]+\\.[A-Za-z]{2,}$", message = "Vui lòng nhập đúng định dạng email")
+    @RuleAnnotation.ExistedEmail( message = "{message.login.invalid_email}")
+    @NotBlank(message = "{validation.required.email}")
+    @Size(max = 255, message = "{validation.input.max_length.255}")
+    @Pattern(regexp = "[\\w.%+-]+@[\\w.-]+\\.[A-Za-z]{2,}$", message = "{message.login.format_email}")
     private String email;
 
-    @NotBlank(message = "Họ và tên không được để trống")
-    @Size(max = 255, message = "Chỉ nhập tối đa 255 kí tự")
+    @NotBlank(message = "{validation.required.full_name}")
+    @Size(max = 255, message = "{validation.input.max_length.255}")
     private String fullName;
 
-    @NotBlank(message = "Số điện thoại không được để trống")
-    @Size(max = 20, message = "Chỉ nhập tối đa 20 kí tự")
+    @NotBlank(message = "{validation.required.phone}")
+    @Size(max = 20, message = "{validation.input.max_length.20}")
     private String phone;
 
-    @NotBlank(message = "Mật khẩu không được để trống")
-    @Size(min = 6, max = 16, message = "Mật khẩu tối thiểu 6 kí tự và không quá 16 kí tự")
+    @NotBlank(message = "{validation.required.password}")
+    @Size(min = 6, max = 16, message = "{validation.required.password_character}")
     private String password;
 
-    @NotBlank(message = "Xác nhận mật khẩu không được để trống")
+    @NotBlank(message = "{validation.required.password_confirmation}")
     private String passwordConfirmation;
 
-    @NotBlank(message = "Vai trò không được để trống")
+    @NotBlank(message = "{validation.required.role}")
     private String roleId;
 }

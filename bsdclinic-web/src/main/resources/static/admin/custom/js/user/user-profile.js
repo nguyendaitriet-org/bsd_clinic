f01671ef5a1b2aa087af32f968b797df7c26a6a9
nguyendaitriet96 <triet.nguyendai@smartdev.com>
2025-05-14 15:44:32 +0700

    BSD-51: Update user profile UI

--- bsdclinic-web/src/main/resources/static/admin/custom/js/user/user-profile.js
+++ bsdclinic-web/src/main/resources/static/admin/custom/js/user/user-profile.js
@@ -2,8 +2,6 @@ export const UserProfile = (function () {
     const module = {
         editIconSelector: $('.edit-icon'),
         cancelButtonSelector: $('.cancel-btn')
-
-
     };
 
     module.init = () => {
@@ -12,45 +10,27 @@ export const UserProfile = (function () {
     }
 
     const handleEditIcon = () => {
-        console.log('handleEditIcon')
         module.editIconSelector.on('click', function () {
             $(this).closest('div').siblings('.input-text').prop('hidden', true);
             $(this).closest('div').siblings('.edit-input').prop('hidden', false);
         });
     }
 
-
-        /*
-        * Hiện lại tất cả các ô input text có giá trị cũ
-        * Ẩn tất cả các ô edit input và thay lại bằng giá trị cũ
-        * */
+    /*
+    * Hiện lại tất cả các ô input text có giá trị cũ
+    * Ẩn tất cả các ô edit input và thay lại bằng giá trị cũ
+    * */
     const handleCancelButton = () => {
-        $(document).on('click', '.cancel-btn', function () {
+        module.cancelButtonSelector.on('click', function () {
             $('.edit-input').prop('hidden', true);
             $('.input-text').prop('hidden', false);
             $('.edit-input').each(function () {
                 const input = $(this).find('input');
-                const originalValue = $(this).siblings('.input-text').text().trim();
+                const originalValue = $(this).siblings('.input-text').text();
                 input.val(originalValue);
             });
         });
     };
-
-    module.editField  = (fieldId) => {
-        const field = document.getElementById(fieldId);
-        const value = field.textContent.trim();
-        const input = document.createElement('input');
-        input.type = 'text';
-        input.value = value;
-        input.className = 'form-control form-control-sm';
-        input.onblur = function () {
-            field.innerText = this.value;
-            field.setAttribute("data-updated", "true");
-        };
-        field.innerText = '';
-        field.appendChild(input);
-        input.focus();
-    }
 
     return module;
 })();

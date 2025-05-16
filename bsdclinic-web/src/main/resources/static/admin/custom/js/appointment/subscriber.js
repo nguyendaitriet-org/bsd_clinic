cea0a84eceeb7088c6690319a1ff262de2a9a4d8
DESKTOP-6LG62K8\Triet_PC <triet9510@gmail.com>
2025-05-16 22:03:07 +0700

    BSD-82: Build appoitment list modal

---
+++ bsdclinic-web/src/main/resources/static/admin/custom/js/appointment/subscriber.js
@@ -0,0 +1,14 @@
+import {App} from "/common/js/app.js";
+import {FormHandler} from "/common/js/form.js";
+
+export const Subscriber = (function () {
+    const module = {
+        subscriberListTableSelector: $('#subscriber-list-table')
+    };
+
+    module.init = () => {
+
+    }
+
+    return module;
+})();

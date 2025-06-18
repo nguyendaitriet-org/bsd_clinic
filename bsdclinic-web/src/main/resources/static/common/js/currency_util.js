0c4e66821a24a411ad5fbe5d2dc7b00e78260c44
DESKTOP-6LG62K8\Triet_PC <triet9510@gmail.com>
2025-06-18 23:04:14 +0700

    BSD-139: Integrate medical record updating API

--- bsdclinic-web/src/main/resources/static/common/js/currency_util.js
+++ bsdclinic-web/src/main/resources/static/common/js/currency_util.js
@@ -5,5 +5,20 @@ export const CurrencyConverter = (function () {
         return new Intl.NumberFormat('vi-VN').format(amount) + ' đ';
     }
 
+    // Format price input with thousands separators
+    module.setupPriceFormatter = (inputSelector) => {
+        inputSelector.on('input', function () {
+            let value = $(this).val().replace(/\D/g, '');
+            value = value.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+            $(this).val(value);
+        });
+    }
+
+    // Get numeric value from formatted input
+    module.getNumericValue = (inputSelector) => {
+        let raw = inputSelector.val().replace(/\./g, '');
+        return parseInt(raw, 10) || 0;
+    }
+
     return module;
 })();

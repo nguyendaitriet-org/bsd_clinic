9a095802c31c1a8337e4faaccafaa36dc5a9ca79
DESKTOP-6LG62K8\Triet_PC <triet9510@gmail.com>
2025-08-14 10:28:13 +0700

    BSDS-35: Integrate category creation API

--- bsdclinic-web/src/main/resources/static/admin/custom/js/category/component.js
+++ bsdclinic-web/src/main/resources/static/admin/custom/js/category/component.js
@@ -5,9 +5,12 @@ export const CategoryComponent = (function () {
         `<div class="col-md-6 col-lg-4 mb-3 category-item-create">
             <div class="card h-100">
                 <div class="card-body">
-                    <div class="d-flex justify-content-between gap-3">
-                        <input type="text" class="category-title form-control form-control-sm"/>
+                    <div class="d-flex justify-content-between gap-3 with-validation">
+                        <input type="text" class="category-title-input form-control form-control-sm" name="title"/>
                         <div class="btn-group">
+                            <button class="btn btn-sm btn-success save-category-btn" type="button">
+                                <i class="fas fa-check"></i>
+                            </button>
                             <button class="btn btn-sm btn-danger cancel-category-btn" type="button">
                                 <i class="fas fa-times"></i>
                             </button>

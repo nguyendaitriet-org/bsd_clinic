1c207125319c930bb02b032ae04211d0f72e6bbf
DESKTOP-6LG62K8\Triet_PC <triet9510@gmail.com>
2025-08-24 16:33:37 +0700

    BSDS-43: Add category to medical service creation form

--- bsdclinic-web/src/main/java/com/bsdclinic/controller/MedicalServiceController.java
+++ bsdclinic-web/src/main/java/com/bsdclinic/controller/MedicalServiceController.java
@@ -1,14 +1,31 @@
 package com.bsdclinic.controller;
 
+import com.bsdclinic.CategoryService;
 import com.bsdclinic.RoleAuthorization;
+import com.bsdclinic.category.CategoryType;
+import com.bsdclinic.dto.request.CategoryListRequest;
+import com.bsdclinic.dto.response.CategoryResponse;
 import com.bsdclinic.url.WebUrl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class MedicalServiceController {
+    private final CategoryService categoryService;
+
+    @ModelAttribute("medicalServiceCategories")
+    public List<CategoryResponse> getMedicineCategories() {
+        CategoryListRequest request = CategoryListRequest.builder()
+                .categoryType(CategoryType.MEDICAL_SERVICE.name())
+                .build();
+        return categoryService.getCategories(request);
+    }
+
     @RoleAuthorization.AdminAuthorization
     @GetMapping(WebUrl.ADMIN_MEDICAL_SERVICE_INDEX)
     public String toIndex() {

a9591d018008f3af4253ee52e36d01f5ebab82d2
DESKTOP-6LG62K8\Triet_PC <triet9510@gmail.com>
2025-10-12 18:25:50 +0700

    BSDS-53: Build resource creation form

--- bsdclinic-web/src/main/java/com/bsdclinic/controller/admin/ResourceController.java
+++ bsdclinic-web/src/main/java/com/bsdclinic/controller/admin/ResourceController.java
@@ -1,6 +1,10 @@
 package com.bsdclinic.controller.admin;
 
+import com.bsdclinic.CategoryService;
 import com.bsdclinic.RoleAuthorization;
+import com.bsdclinic.category.CategoryType;
+import com.bsdclinic.dto.request.CategoryListRequest;
+import com.bsdclinic.dto.response.CategoryResponse;
 import com.bsdclinic.message.MessageProvider;
 import com.bsdclinic.url.WebUrl;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +12,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 public class ResourceController {
     private final MessageProvider messageProvider;
+    private final CategoryService categoryService;
 
     @ModelAttribute("resourceTypes")
     public Map<String, String> getCategoryTypes() {
         return messageProvider.getMessageMap("resource.type", "constants");
+    }
+
+    @ModelAttribute("entityCategories")
+    public List<CategoryResponse> getMedicineCategories() {
+        CategoryListRequest request = CategoryListRequest.builder()
+                .categoryType(CategoryType.RESOURCE.name())
+                .build();
+        return categoryService.getCategories(request);
     }
 
     @RoleAuthorization.AdminAuthorization
